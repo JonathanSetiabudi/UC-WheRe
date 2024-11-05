fc1b95e32ec2b714ae2c76d1012c4c263077c76c
Charleen Chen <charleenc485@gmail.com>
2024-11-05 00:07:58 -0800

    Added socket stuff

---
+++ server/index.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const app = express();
+const http = require("http");
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+    credentials: true,
+    transports: ["websocket", "polling"],
+  },
+});
+
+io.on("connection", (socket) => {
+  socket.on("join", (room) => {
+    socket.join(room);
+  });
+
+  socket.on("leave", (room) => {
+    socket.leave(room);
+  });
+
+  socket.on("message", ({ message }) => {
+    console.log("I AM BEING RECIEVED", message)
+    socket.to(123).emit("message", message);
+  });
+});
+
+server.listen(8080, () => {
+  console.log("listening on *:8080");
+});
