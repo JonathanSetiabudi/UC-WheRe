f24ee2c04d533460933eef38c5be05ad27ffd221
anitathakim <anitathakim@gmail.com>
2024-11-16 14:52:11 -0800

    made page.tsx for game

---
+++ src/app/game/page.tsx
@@ -0,0 +1,57 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import { socket } from "@/utils/socket";
+import Location from "../objects/Location.js";
+
+// export default function Game() {
+//   socket.connect();
+
+
+// }
+
+interface GameProps {
+  locations: Location[];
+}
+
+const Game = ({ locations } : GameProps) => {
+  const [cardsList, setCardsList] = useState<Location[]>(locations.slice(0, 16));
+  const [cardsTheme, setCardsTheme] = useState<string>("Residential&Dining");
+
+  const setSize4x4 = () => {
+    if (cardsList.length != 16) {
+      setCardsList(locations.slice(0, 16));
+    }
+  };
+  
+  const setSize4x5 = () => {
+    if (cardsList.length != 20 ) {
+      const addLocations = locations.slice(cardsList.length, 20);
+      setCardsList([...cardsList, ...addLocations]);
+    }
+  };
+
+  const filterTheme = (theme: string) => {
+    setCardsTheme(theme);
+    const filteredLocations = locations.filter(location => location.locationType === theme);
+  }
+
+  return (
+    <div>
+
+    {/* Gridsize buttons */}
+    <button onClick={setSize4x4}>Set Size to 16 (4x4)</button>
+    <button onClick={setSize4x5}>Expand to 20 (4x5)</button>
+
+    {/* Location theme dropdown menu */}
+    <select onChange={(e) => filterTheme(e.target.value)} value={cardsTheme}>
+      <option value="Residential&Dining">Residential & Dining</option>
+      <option value="CampusLandmarks">Campus Landmarks</option>
+      <option value="StudySpots">Study Spots</option>
+      <option value="BikeRacks">Bike Racks</option>
+      <option value="Streets&ParkingLots">Streets and Parking Lots</option>
+
+    </select>
+  </div>
+  )
+};
