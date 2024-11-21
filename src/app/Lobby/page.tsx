26c44769154e172f94e12837ebec700afae39999
adidi-c <mradidi18@gmail.com>
2024-11-21 13:34:53 -0800

    fixed lint errors for lobby/page.tsx

--- src/app/Lobby/page.tsx
+++ src/app/Lobby/page.tsx
@@ -1,12 +1,14 @@
 "use client";
 
-import React, { Component, useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 import Home from "../page.jsx";
 import { socket } from "@/utils/socket";
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 import userRooms from ".../server/index.js";
 
-const Lobby = ({ code }) => {
-  // const [code, setCode] = useState(code);
+const Lobby = ({}) => {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const [playersInLobby, setPlayerCount] = useState(0);
   const [boardDifficulty, setDifficulty] = useState(0);
   const [boardTheme, setTheme] = useState(1);
@@ -14,9 +16,11 @@ const Lobby = ({ code }) => {
   const [gridSize, setGridSize] = useState(16);
 
   useEffect(() => {
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
     socket.on("createdLobby", (room) => {
       setPlayerCount((playersInLobby) => playersInLobby + 1);
     });
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
     socket.on("joinedLobby", (room) => {
       setPlayerCount((playersInLobby) => playersInLobby + 1);
     });
@@ -44,68 +48,54 @@ const Lobby = ({ code }) => {
   };
 
   // difficulty handlers
-  const handleClickEasy = (event: React.MouseEvent<HTMLButtonElement>) => {
+  const handleClickEasy = () => {
     onDifficultyChange(0);
   };
 
-  const handleClickMedium = (event: React.MouseEvent<HTMLButtonElement>) => {
+  const handleClickMedium = () => {
     onDifficultyChange(1);
   };
 
-  const handleClickHard = (event: React.MouseEvent<HTMLButtonElement>) => {
+  const handleClickHard = () => {
     onDifficultyChange(2);
   };
 
   // theme handlers
-  const handleClickThemeResAndDining = (
-    event: React.MouseEvent<HTMLButtonElement>,
-  ) => {
+  const handleClickThemeResAndDining = () => {
     onThemeChange(0);
   };
 
-  const handleClickThemeCampusLandmarks = (
-    event: React.MouseEvent<HTMLButtonElement>,
-  ) => {
+  const handleClickThemeCampusLandmarks = () => {
     onThemeChange(1);
   };
 
-  const handleClickThemeStudySpots = (
-    event: React.MouseEvent<HTMLButtonElement>,
-  ) => {
+  const handleClickThemeStudySpots = () => {
     onThemeChange(2);
   };
 
-  const handleClickThemeBikeRacks = (
-    event: React.MouseEvent<HTMLButtonElement>,
-  ) => {
+  const handleClickThemeBikeRacks = () => {
     onThemeChange(3);
   };
 
-  const handleClickThemeStreetsAndParking = (
-    event: React.MouseEvent<HTMLButtonElement>,
-  ) => {
+  const handleClickThemeStreetsAndParking = () => {
     onThemeChange(4);
   };
 
   // guess handlers (will probably convert to dropdown menu)
-  const handleClickGuess1 = (event: React.MouseEvent<HTMLButtonElement>) => {
+  const handleClickGuess1 = () => {
     onNumGuessChange(1);
   };
 
-  const handleClickGuess3 = (event: React.MouseEvent<HTMLButtonElement>) => {
+  const handleClickGuess3 = () => {
     onNumGuessChange(3);
   };
 
   // board size handlers (will probably convert this to dropdown as well)
-  const handleClickBoardSmall = (
-    event: React.MouseEvent<HTMLButtonElement>,
-  ) => {
+  const handleClickBoardSmall = () => {
     onGridChange(16);
   };
 
-  const handleClickBoardLarge = (
-    event: React.MouseEvent<HTMLButtonElement>,
-  ) => {
+  const handleClickBoardLarge = () => {
     onGridChange(20);
   };
 
